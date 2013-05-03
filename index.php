080e80bfcb87bb62975c1f9360182a8507edb471
Awakened <awakened@awakened.(none)>
2013-05-03 18:07:03 +0530

    tango

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 date_default_timezone_set('UTC');
 
 require('./libs/Client.php');
+require('./libs/rssreader.php');
 require('./libs/SocialCounter.php');
 require_once('./php/autoloader.php');
 
@@ -17,21 +18,28 @@ $url = '';
 if (isset($_GET['url'])) {
   $url = $_GET['url'];
 
-  $counter = new SocialCounter($client, $simple_pie);
-  $items = $counter->getItems($url, 20);
+  $reader = new rssreader($client, $simple_pie);
+  $items = $reader->getItems($url, 20);
 
-  $items = $counter->getTweetCount($items);
-  $items = $counter->getFBCount($items);
-  $items = $counter->getSUCount($items);
+  //$items = $reader->getTweetCount($items);
+  //$items = $reader->getFBCount($items);
+  //$items = $reader->getSUCount($items);
+  
   //pr($items);die();
 
-  if (!$counter->rss_or_xml) {
+  if (!$reader->rss_or_xml) {
     // We must suggest something like
     // "Do you want to test [RSS Link] ?"
     // For that getting the Feeds URL is important
 
     // '@(https?://\S+?feed\S+)@ui' ???
   }
+}
+
+if(isset($_POST['get_count_for']) && isset($_POST['service']))
+{
+	$url = $_POST['get_count_for'];
+	$service = $_POST['service'];
 }
 
 require('./views/home.php');
