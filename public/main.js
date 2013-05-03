779e3a9fd27e5a4e6b6861d53f2622c3cffc9586
Awakened <awakened@awakened.(none)>
2013-05-03 18:24:10 +0530

    tango

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@ $("table tr").each(function(e){
 	var $tr = $(this);
 	$url = $tr.find(".item_url").attr("href");
 	$tr.find(".twitter_count").html("loading...");
-	$.post("", {get_count_for: $url, service: 'twitter'}, function(data){
+	$.post("index.php", {get_count_for: $url, service: 'twitter'}, function(data){
 		$tr.find(".twitter_count").html(data);
 	})
 });
