1c72238fb17c297561981bdc294806cf07663375
Awakened <awakened@awakened.(none)>
2013-05-03 19:22:34 +0530

    tango

--- public/main.js
+++ public/main.js
@@ -7,36 +7,24 @@ $("table tr").each(function(e){
 	var $tr = $(this);
 	$url = $tr.find(".item_url").attr("href");
 	//twitter
-	$tr.find(".twitter_count").html("loading...");
 	$.get("index.php", {get_count_for: $url, service: 'twitter'}, function(data){
 		$loaded++;
-		if(data != "0")
-		{
-			$tr.find(".twitter_count").html(data);
-			highest_twitter = Math.max(parseInt(highest_twitter), parseInt(data));
-		}
+		$tr.find(".twitter_count").html(data);
+		highest_tw = Math.max(parseInt(highest_tw), parseInt(data));
 		apply_opacity();
 	});
 	
 	//facebook
-	$tr.find(".facebook_count").html("loading...");
 	$.get("index.php", {get_count_for: $url, service: 'facebook'}, function(data){
 		$loaded++;
-		if(data != "0")
-		{
-			$tr.find(".facebook_count").html(data);
-		}
+		$tr.find(".facebook_count").html(data);
 		apply_opacity();
 	});
 	
 	//stumbleupon
-	$tr.find(".stumbleupon_count").html("loading...");
 	$.get("index.php", {get_count_for: $url, service: 'stumbleupon'}, function(data){
 		$loaded++;
-		if(data != "0")
-		{
-			$tr.find(".stumbleupon_count").html(data);
-		}
+		$tr.find(".stumbleupon_count").html(data);
 		apply_opacity();
 	});
 });
