466e175231f4151fa41e5a4e884c2dc0351c78f8
Awakened <awakened@awakened.(none)>
2013-05-03 19:35:27 +0530

    tango

--- public/main.js
+++ public/main.js
@@ -4,34 +4,32 @@ console.log($loaded);
 var highest_fb = 0;
 var highest_tw = 0;
 var highest_su = 0;
-$("table tr").each(function(e){
+$("table tr:not(:first-child)").each(function(e){
 	var $tr = $(this);
 	$url = $tr.find(".item_url").attr("href");
-	if($url != "")
-	{
-		//twitter
-		$.get("index.php", {get_count_for: $url, service: 'twitter'}, function(data){
-			$loaded++;
-			$tr.find(".twitter_count").html(data);
-			console.log(parseInt(highest_tw)+", "+parseInt(data))
-			highest_tw = Math.max(parseInt(highest_tw), parseInt(data));
-			apply_opacity();
-		});
-	
-		//facebook
-		$.get("index.php", {get_count_for: $url, service: 'facebook'}, function(data){
-			$loaded++;
-			$tr.find(".facebook_count").html(data);
-			apply_opacity();
-		});
-	
-		//stumbleupon
-		$.get("index.php", {get_count_for: $url, service: 'stumbleupon'}, function(data){
-			$loaded++;
-			$tr.find(".stumbleupon_count").html(data);
-			apply_opacity();
-		});
-	}
+
+	//twitter
+	$.get("index.php", {get_count_for: $url, service: 'twitter'}, function(data){
+		$loaded++;
+		$tr.find(".twitter_count").html(data);
+		console.log(parseInt(highest_tw)+", "+parseInt(data))
+		highest_tw = Math.max(parseInt(highest_tw), parseInt(data));
+		apply_opacity();
+	});
+
+	//facebook
+	$.get("index.php", {get_count_for: $url, service: 'facebook'}, function(data){
+		$loaded++;
+		$tr.find(".facebook_count").html(data);
+		apply_opacity();
+	});
+
+	//stumbleupon
+	$.get("index.php", {get_count_for: $url, service: 'stumbleupon'}, function(data){
+		$loaded++;
+		$tr.find(".stumbleupon_count").html(data);
+		apply_opacity();
+	});
 });
 
 function apply_opacity()
